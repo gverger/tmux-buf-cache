8881f1f1854977d70de5837923de3c56325fdd40
Guillaume Verger <verger.guillaume@gmail.com>
2022-04-22 14:11:08 +0200

    change color

--- scripts/used_buff_cache.sh
+++ scripts/used_buff_cache.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+high_usage ()
+{
+    cache=$(free --giga | grep Mem | awk '{ print $6 }')
+    if [[ $cache -ge 5 ]]; then
+        printf "#[bg=black,fg=red]"
+    else
+        printf "#[bg=black,fg=green]"
+    fi
+}
+
 used_buf_cache ()
 {
     cache=$(free -h | grep Mem | awk '{ print $6 }')
@@ -8,6 +18,7 @@ used_buf_cache ()
 
 main ()
 {
+    high_usage
     used_buf_cache
 }
 
